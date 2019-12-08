0cf215a69e1019e82f4d510cee06ec2f182d3d02
Drew Johnson <drew.johnson@mangahigh.com>
2019-12-08 08:54:46 +0000

    Day 8 part 1

--- day_8/index.js
+++ day_8/index.js
@@ -1,3 +1,55 @@
+function corruptionCheck(input, w, h) {
+  const layers = getLayers(input, w, h);
+
+  let result = 0;
+  let minZeroCount = Number.POSITIVE_INFINITY;
+
+  Object.values(layers).forEach((layer) => {
+    const zeroCount = getCharCount(layer, '0');
+    if (zeroCount < minZeroCount) {
+      result = getCharCount(layer, '1') * getCharCount(layer, '2');
+      minZeroCount = zeroCount
+    }
+  });
+
+  return result;
+}
+
+function getLayers (input, w, h) {
+  let workingInput = input;
+  const layers = {}
+  let layerId = 1;
+  let rowId = 1;
+  while(workingInput.length) {
+
+    const rowString = workingInput.substr(0, w);
+
+    layers[layerId] = layers[layerId] || '';
+    layers[layerId] += rowString;
+
+    if (rowId === h) {
+      rowId = 1;
+      layerId += 1
+    } else {
+      rowId += 1;
+    }
+
+    workingInput = workingInput.slice(w);
+  }
+
+  return layers;
+}
+
+function getCharCount(string, char) {
+  return string
+    .split('')
+    .filter(c => c === `${char}`)
+    .length;
+}
+
+
 module.exports = {
   getLayers,
+  getCharCount,
+  corruptionCheck,
 }
--- day_8/index.test.js
+++ day_8/index.test.js
@@ -1,18 +1,47 @@
 const { expect } = require('chai');
-const { getLayers }  from './index'
+const { getLayers, getCharCount, corruptionCheck }  = require('./index')
+const fs = require('fs');
 
 describe.only('day 8 ', () => {
   describe('getLayers', () => {
     it('extracts the test data', () => {
       const input = '123456789012';
-      const height = 2;
       const width = 3;
-      const result  getLayers(input, width, height);
+      const height = 2;
+      const result = getLayers(input, width, height);
 
       expect(result).to.deep.equal({
         1: '123456',
-        2: '789123',
+        2: '789012',
       })
+    });
+  });
+
+  describe('corruptionCheck', () => {
+    it('works with the test data', () => {
+      const input = '123456789012';
+      const width = 3;
+      const height = 2;
+      const result = corruptionCheck(input, width, height);
+
+      expect(result).to.equal(1);
+    });
+
+    it('solves part 1', () => {
+      const data = fs.readFileSync('./day_8/data.txt', 'utf8').trim();
+
+      const width = 25;
+      const height = 6;
+      const result = corruptionCheck(data, width, height);
+
+      expect(result).to.equal(1);
+    });
+  });
+
+  describe('getCharCount', () => {
+    it('returns the count for a given char', () => {
+      const string = '0011223';
+      expect(getCharCount(string, '0')).to.equal(2);
     });
   });
 });
