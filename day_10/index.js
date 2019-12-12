a366949f3a915c1de389fcfba477c060c42636cb
Drew Johnson <drew.johnson@mangahigh.com>
2019-12-12 10:45:23 +0000

    Scrape through day 10

--- day_10/index.js
+++ day_10/index.js
@@ -1,10 +1,291 @@
-function getAsteroidCount(gridData, point) {
-  const [x,y] = point;
-  grid = gridData.split('\n').map(row => row.split(''));
+const Reset = '\x1b[0m';
+const Bright = '\x1b[1m';
+const Dim = '\x1b[2m';
+const Underscore = '\x1b[4m';
+const Blink = '\x1b[5m';
+const Reverse = '\x1b[7m';
+const Hidden = '\x1b[8m';
 
-  console.log({grid});
+const FgBlack = '\x1b[30m';
+const FgRed = '\x1b[31m';
+const FgGreen = '\x1b[32m';
+const FgYellow = '\x1b[33m';
+const FgBlue = '\x1b[34m';
+const FgMagenta = '\x1b[35m';
+const FgCyan = '\x1b[36m';
+const FgWhite = '\x1b[37m';
+
+const BgBlack = '\x1b[40m';
+const BgRed = '\x1b[41m';
+const BgGreen = '\x1b[42m';
+const BgYellow = '\x1b[43m';
+const BgBlue = '\x1b[44m';
+const BgMagenta = '\x1b[45m';
+const BgCyan = '\x1b[46m';
+const BgWhite = '\x1b[47m';
+
+const OBJECTS = {
+  ASTEROID: '#',
+};
+
+function getNthAsteroid(gridData, baseCoords, n, { render, renderOrder } = {}) {
+  const asteroids = getAsteroids(gridData);
+
+  const renderOutputs = [];
+  const gridWidth = gridData.split('\n')[0].length;
+  const [ baseX, baseY ] = baseCoords;
+
+  const linesFromBasePoint = {};
+  let renderOutput = gridData;
+  // TODO: there has ro
+  let gradsRight = [];
+  let gradsLeft = [];
+
+  // set up the map of asteroids on each line
+  for (let coords in asteroids) {
+    const [ asteroidX, asteroidY ] = coords.split(',').map(toInt);
+    if (asteroidX === baseX && asteroidY === baseY) continue;
+
+    const vector = [ asteroidX - baseX, baseY - asteroidY ];
+    const manhattanDistance = Math.abs(vector[0]) + Math.abs(vector[1]);
+    const gradient = vector[1] / vector[0];
+
+    const vectorDirection = vector.map(n => Math.sign(n));
+    const vectorString = [ `x:${vectorDirection[0] || 1},g:${gradient}`].join(',');
+
+    if (vectorDirection[0] >= 0) {
+      if (!gradsRight.includes(gradient)) gradsRight.push(gradient);
+    } else {
+      if (!gradsLeft.includes(gradient)) gradsLeft.push(gradient);
+    }
+    // const vectorStr = vector.join('');
+    if (!linesFromBasePoint[vectorString]) {
+      linesFromBasePoint[vectorString] = {
+        [manhattanDistance]: coords,
+      };
+    } else {
+      linesFromBasePoint[vectorString] = {
+        ...linesFromBasePoint[vectorString],
+        [manhattanDistance]: coords,
+      };
+    }
+  }
+
+  gradsRight.sort((a,b) => a > b ? -1 : 1);
+  gradsLeft.sort((a,b) => a > b ? -1 : 1);
+
+  let totalGradientLength = gradsRight.length + gradsLeft.length;
+  let count = 0;
+  let interval = 0;
+  let result;
+
+  // loop through the lines
+  while (count < (n || totalGradientLength) && totalGradientLength) {
+    let vx;
+    let gradients;
+    let gradientIndex;
+    const cycle = (count % 9) + 1;
+
+    let vectors = {};
+    let vectorKey;
+
+    const loopedInterval = interval % totalGradientLength;
+
+    // check left or right side?
+    if (loopedInterval < gradsRight.length) {
+      vx = 1;
+      gradients = gradsRight;
+      gradientIndex = loopedInterval;
+    } else {
+      vx = -1;
+      gradients = gradsLeft;
+      gradientIndex = loopedInterval - gradsRight.length;
+    }
+    const gradToTry = gradients[gradientIndex];
+    vectorKey = `x:${vx},g:${gradToTry}`;
+    vectors = linesFromBasePoint[vectorKey];
+
+    // If that line is empty
+    // remove the gradient so we don't check it again
+    // adjust the interval so it is still relatively correct
+    if (!Object.keys(vectors).length) {
+      console.log('skipping');
+      gradients.splice(loopedInterval, 1);
+
+      totalGradientLength--;
+
+      if (totalGradientLength === 0) {
+
+        if (render) {
+          renderOutputs.push(renderOutput);
+        }
+        break;
+      }
+
+
+      continue;
+    }
+
+
+    const nearestDistance = Math.min(...Object.keys(vectors).map(toInt));
+    const coords = linesFromBasePoint[vectorKey][nearestDistance];
+
+    const numericCoords = stringToPoint(coords);
+    const renderOutputIndex = +numericCoords[0] + (numericCoords[1] * (gridWidth + 1));
+
+    if (render) {
+      renderOutput = renderOutput.substr(0, renderOutputIndex) + `${cycle}` + renderOutput.substr(renderOutputIndex + 1);
+    }
+
+    if (count === n - 1) {
+      result = coords;
+      if (render) {
+        renderOutputs.push(renderOutput);
+      }
+      break;
+    }
+
+    delete linesFromBasePoint[vectorKey][nearestDistance];
+
+    if (render && cycle === 9) {
+      renderGrid(renderOutput, asteroids, baseCoords);
+      renderOutputs.push(renderOutput);
+      renderOutput = renderOutput.replace(/[1-9]/g, '.');
+    }
+
+
+    count++;
+    interval++;
+  }
+
+  if (render) {
+    renderGrid(renderOutput, asteroids, baseCoords);
+    return renderOutputs;
+  } else {
+    return result;
+  }
+}
+
+function getMaxVisibility(gridData) {
+  const asteroids = getAsteroids(gridData);
+
+  for (let coords in asteroids) {  // loop in a loop!
+    const asteroidCoord = stringToPoint(coords);
+    asteroids[asteroidCoord].visible = getAsteroidCount(asteroids, asteroidCoord);
+  }
+  const max = Object.values(asteroids).map(a => a.total).reduce((a,b) => Math.max(a,b), 0);
+  const basePoint = Object.keys(asteroids)
+    .find((coord) => asteroids[coord].total === max);
+
+  console.log({basePoint});
+  renderGrid(gridData, asteroids, stringToPoint(basePoint));
+  return max;
+}
+
+
+function getAsteroids(gridData) {
+  const grid = gridData.split('\n').map(row => row.split(''));
+  const asteroids = {};
+  console.log(grid.length);
+  // Build asteroid register
+  grid.forEach((row, yIndex) => {
+    row.forEach((cell, xIndex) => {
+      if (grid[yIndex][xIndex] === OBJECTS.ASTEROID) {
+        asteroids[`${xIndex},${yIndex}`] = {
+          visible: 0,
+        };
+      }
+    });
+  });
+
+  return asteroids;
+}
+
+/**
+ * Get the visible
+ * @param  {[type]} gridData [description]
+ * @param  {[type]} point    [description]
+ * @return {[type]}          [description]
+ */
+function getAsteroidCount(asteroids, point, returnData) {
+  const [baseX, baseY] = point;
+  const basePositionString = point.join(',');
+
+  const vectorCounts = {};
+
+  for (let coords in asteroids) {
+    const [ asteroidX, asteroidY ] = coords.split(',').map(toInt);
+
+    if (asteroidX === baseX && asteroidY === baseY) continue;
+
+    const vector = [ asteroidX - baseX, asteroidY - baseY ];
+    const gradient = vector[1] / vector[0];
+    const vectorDirection = vector.map(n => Math.sign(n));
+    const vectorString = [ vectorDirection[0], vectorDirection[1], gradient ];
+
+    if (!vectorCounts[vectorString]) vectorCounts[vectorString] = 0;
+    vectorCounts[vectorString] += 1;
+  }
+
+  const total = Object.keys(vectorCounts).length;
+
+  asteroids[basePositionString] = {
+    ...asteroids[basePositionString],
+    total,
+    vectorCounts,
+  };
+
+  if (returnData) {
+    return asteroids[basePositionString];
+  }
+  return asteroids[basePositionString].total;
+}
+
+
+const toInt = v => +v;
+const stringToPoint = (string) => string.split(',').map(toInt);
+
+const logJson = (logged, name) => {
+  Object.keys(logged).forEach((name) => {
+    thisObj = logged[name];
+
+    console.log(name, JSON.stringify(thisObj, null, 2));
+  });
+};
+
+function renderGrid(gridData, asteroids, basePoint) {
+  const grid = gridData.split('\n').map(row => row.split(''));
+  const gridWidth = grid[0].length;
+  let outStr = '\n/' + Array(gridWidth + 2).fill('-').join('') + '\\\n';
+
+  let basePointStr = basePoint.join(',');
+
+
+  grid.forEach((row, yIndex) => {
+    outStr += '| ';
+    row.forEach((cell, xIndex) => {
+
+      const coordString = [xIndex, yIndex].join(',');
+
+      if (coordString === basePointStr) outStr += `${BgWhite}${FgBlack}B${Reset}`;
+      else if ('123456789'.indexOf(cell) > -1) outStr += `${Bright}${cell}${Reset}`;
+      else if (cell === '#' && asteroids[coordString]) outStr += `${FgRed}#${Reset}`;
+      else outStr += `${BgBlack} ${Reset}`;
+    });
+
+    outStr += ' |';
+    outStr += '\n';
+  });
+
+
+  outStr += '\\' + Array(gridWidth + 2).fill('-').join('') + '/';
+  outStr += '\n';
+  console.log(outStr);
 }
 
 module.exports = {
+  getAsteroids,
   getAsteroidCount,
-}
+  getMaxVisibility,
+  getNthAsteroid,
+};
--- day_10/index.test.js
+++ day_10/index.test.js
@@ -1,18 +1,308 @@
 const { expect } = require('chai');
-const { getAsteroidCount }  = require('./index');
-// const data = require('./data');
+const { getAsteroids, getAsteroidCount, getMaxVisibility, getNthAsteroid, showNAsteroids }  = require('./index');
+const data = require('./data');
 
-describe.only('getAsteroidCount', () => {
-  it('detects adjacent asteroids on the horizontal', () => {
-    const input = `
-      .#
-      ..`
-    .replace(/\s/g, '');
-    console.log({input});
+const makeMap = str => str.trim().split('\n').map(row => row.trim()).join('\n');
 
-    const result = getAsteroidCount(input, [0,0]);
-    expect(result).to.equal(1);
+describe.only('day 10', () => {
+  describe('getAsteroidCount', () => {
+    it('detects adjacent asteroids on the horizontal', () => {
+      const input = makeMap(`
+##
+..`);
 
+      const result = getAsteroidCount(getAsteroids(input), [0,0]);
+      expect(result).to.equal(1);
+    });
 
+    it('detects adjacent asteroids on the horizontal', () => {
+      const input = makeMap(`
+.#
+.#`);
+      const result = getAsteroidCount(getAsteroids(input), [1,0]);
+      expect(result).to.equal(1);
+    });
+
+    it('detects adjacent asteroids on the diagonal', () => {
+      const input = makeMap(`
+.#
+#.`);
+      const result = getAsteroidCount(getAsteroids(input), [1,0]);
+      expect(result).to.equal(1);
+    });
+
+    it('detects adjacent asteroids on non-1 vectors', () => {
+      const input = makeMap(`
+.#
+..
+#.`);
+      const result = getAsteroidCount(getAsteroids(input), [1,0]);
+      expect(result).to.equal(1);
+    });
+
+    it('detects and ignores obscured asteroids', () => {
+      const input = makeMap(`
+.#
+.#
+.#`);
+      const result = getAsteroidCount(getAsteroids(input), [1,0]);
+      expect(result).to.equal(1);
+    });
+
+    it('detects and ignores obscured asteroids in multiple directions', () => {
+      const input = makeMap(`
+###
+#..
+#..`);
+      const result = getAsteroidCount(getAsteroids(input), [0,0]);
+      expect(result).to.equal(2);
+    });
+
+    it('detects and ignores obscured asteroids in opposite directions', () => {
+      const input = makeMap(`
+#####`);
+      const result = getAsteroidCount(getAsteroids(input), [2,0]);
+      expect(result).to.equal(2);
+    });
   });
+
+  describe('getBase', () => {
+    it('solves test case 1', () => {
+      const input = makeMap(`
+.#..#
+.....
+#####
+....#
+...##`);
+
+      const result = getMaxVisibility(input);
+
+      expect(result).to.equal(8);
+    });
+
+    it('solves test case 2', () => {
+      const input = makeMap(`
+......#.#.
+#..#.#....
+..#######.
+.#.#.###..
+.#..#.....
+..#....#.#
+#..#....#.
+.##.#..###
+##...#..#.
+.#....####`);
+
+      const result = getMaxVisibility(input);
+
+      expect(result).to.equal(33);
+    });
+
+    it('solves test case 3', () => {
+      const input = makeMap(`
+#.#...#.#.
+.###....#.
+.#....#...
+##.#.#.#.#
+....#.#.#.
+.##..###.#
+..#...##..
+..##....##
+......#...
+.####.###.`);
+
+      const result = getMaxVisibility(input);
+
+      expect(result).to.equal(35);
+    });
+
+    it('solves test case 4', () => {
+      const input = makeMap(`
+.#..#..###
+####.###.#
+....###.#.
+..###.##.#
+##.##.#.#.
+....###..#
+..#.#..#.#
+#..#.#.###
+.##...##.#
+.....#.#..`);
+
+      const result = getMaxVisibility(input);
+
+      expect(result).to.equal(41);
+    });
+
+    it('solves test case 4', () => {
+      const input = makeMap(`
+.#..##.###...#######
+##.############..##.
+.#.######.########.#
+.###.#######.####.#.
+#####.##.#.##.###.##
+..#####..#.#########
+####################
+#.####....###.#.#.##
+##.#################
+#####.##.###..####..
+..######..##.#######
+####.##.####...##..#
+.#####..#.######.###
+##...#.##########...
+#.##########.#######
+.####.#.###.###.#.##
+....##.##.###..#####
+.#.#.###########.###
+#.#.#.#####.####.###
+###.##.####.##.#..##`);
+
+      const result = getMaxVisibility(input);
+
+      expect(result).to.equal(210);
+    });
+
+    it.only('solves puzzle', () => {
+      const result = getMaxVisibility(makeMap(data));
+      expect(result).to.equal(340);
+    });
+  });
+
+  describe('getNthAsteroid', () => {
+    it('returns the nearest due north first', () => {
+      const input = makeMap(`
+.#.
+.#.
+...
+  `);
+      const result = getNthAsteroid(input, [1,1], 1);
+      expect(result).to.equal('1,0');
+    });
+
+    it('runs clockwise', () => {
+      const input = makeMap(`
+.#.
+.##
+...
+  `);
+
+      // getNthAsteroid(getAsteroids(input), [1,1]);
+      const resultA = getNthAsteroid(input, [1,1], 1);
+      const resultB = getNthAsteroid(input, [1,1], 2);
+      expect(resultA).to.equal('1,0');
+      expect(resultB).to.equal('2,1');
+    });
+
+
+    it.skip('handles test case 1', () => {
+      const input = makeMap(`
+.#....#####...#..
+##...##.#####..##
+##...#...#.#####.
+..#.....X...###..
+..#.#.....#....##
+  `);
+
+      const expected = makeMap(`
+.#....###24...#..
+##...##.13#67..9#
+##...#...5.8####.
+..#.....X...###..
+..#.#.....#....##
+  `);
+
+      const expected2 = makeMap(`
+.#....###.....#..
+##...##...#.....#
+##...#......1234.
+..#.....X...5##..
+..#.9.....8....76`);
+
+      const expected3 = makeMap(`
+.8....###.....#..
+56...9#...#.....#
+34...7...........
+..2.....X....##..
+..1..............
+  `);
+
+      const expected4 = makeMap(`
+......234.....6..
+......1...5.....7
+.................
+........X....89..
+.................`);
+
+
+      const base = [8, 3];
+
+      const images = getNthAsteroid(input, base, 36, { render: true });
+
+      expect(images[0]).to.equal(expected);
+      expect(images[1]).to.equal(expected2);
+      expect(images[2]).to.equal(expected3);
+      expect(images[3]).to.equal(expected4);
+    });
+
+    it('handles blocked lines', () => {
+      const input = makeMap(`
+  .#.
+  .##
+  .#.
+  `);
+
+      const expected = makeMap(`
+  .3.
+  .12
+  .#.
+  `);
+
+      const base = [1, 2];
+
+      const images = getNthAsteroid(input, base, 9, { render: true });
+      expect(images[0]).to.equal(expected);
+
+    });
+
+    it.skip('handles test case 2', () => {
+      const input = makeMap(`
+  .#..##.###...#######
+  ##.############..##.
+  .#.######.########.#
+  .###.#######.####.#.
+  #####.##.#.##.###.##
+  ..#####..#.#########
+  ####################
+  #.####....###.#.#.##
+  ##.#################
+  #####.##.###..####..
+  ..######..##.#######
+  ####.##.####...##..#
+  .#####..#.######.###
+  ##...#.##########...
+  #.##########.#######
+  .####.#.###.###.#.##
+  ....##.##.###..#####
+  .#.#.###########.###
+  #.#.#.#####.####.###
+  ###.##.####.##.#..##`);
+
+      const base = [11,13];
+
+      expect(getNthAsteroid(input, base, 1)).to.equal('11,12');
+      expect(getNthAsteroid(input, base, 2)).to.equal('12,1');
+      expect(getNthAsteroid(input, base, 199)).to.equal('9,6');
+      expect(getNthAsteroid(input, base, 200)).to.equal('8,2');
+      expect(getNthAsteroid(input, base, 201)).to.equal('10,9');
+      expect(getNthAsteroid(input, base, 299)).to.equal('11,1');
+    });
+
+    it('solves the puzzle', () => {
+      const input = makeMap(data);
+      const base = [28,29];
+      // getNthAsteroid(input, base, 207, { render: true });
+      expect(getNthAsteroid(input, base, 200)).to.equal('12,21');
+    });
+  });
+
 });
